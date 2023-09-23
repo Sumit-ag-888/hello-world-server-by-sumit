10e5e11c65d3d444d89e80942c20c0ed1fdfae5f
sumit kumar <sumitagrawal1209@gmail.com>
2023-09-23 11:52:51 +0530

    new commit

--- server.mjs
+++ server.mjs
@@ -1,10 +1,40 @@
 import express from 'express';
-const app = express()
+import cors from 'cors';
+import path from  'path';
+const __dirname = path.resolve();
+
+const app = express();
+app.use(cors())
+
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World! ' + new Date());
+app.get('/profile', (req, res) => {
+  res.send("Hello this is profile");
 })
+
+
+app.get('/weather/:cityName', (req, res) => {
+let userInput = {
+  ramgarh:{
+    temp:28 ,
+    humidity :75,
+  },
+  ranchi:{
+    temp:-46 ,
+    humidity :-9,
+  }
+}
+ let userInputCityName = req.params.cityName.toLowerCase();
+let wetherDatatoSend = userInput[userInputCityName]
+ if(wetherDatatoSend){
+  res.send(wetherDatatoSend);
+ } else {
+  res.status(404).send("message: No Data Found")
+ }
+
+})
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
